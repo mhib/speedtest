a302410d14ce1a8e160d2c94606a354a2ba89a5c
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2015-12-06 16:45:49 +0100

    Minor improvements

--- analyze.rb
+++ analyze.rb
@@ -7,24 +7,34 @@ $regexps = {
 
 Entry = Struct.new(:date, :upload, :download, :ping)
 Hour = Struct.new(:hour, :entries) do
-  $regexps.each_key do |key|
-    define_method "max_#{key}" do
-      entries.max_by { |n| n.send(key) }
+  ['max', 'min'].each do |str|
+    $regexps.each_key do |key|
+      name = "#{str}_#{key}".freeze
+      i_name = "@#{name}".freeze
+      define_method name do
+        if instance_variable_defined?(i_name)
+          instance_variable_get(i_name)
+        else
+          instance_variable_set(i_name, entries.send("#{str}_by") { |n| n.send(key) }.send(key))
+        end
+      end
     end
-  end
-  $regexps.each_key do |key|
-    define_method "min_#{key}" do
-      entries.min_by { |n| n.send(key) }
-    end
-  end
-  $regexps.each_key do |key|
-    define_method "avg_#{key}" do
-      (entries.inject(0) { |m, a| m + a.send(key) } / entries.size).round
-    end
-  end
 
-  def to_s
-    "#{hour}\tPing: #{avg_ping}\tDownload: #{avg_download}\tUpload: #{avg_upload}"
+    $regexps.each_key do |key|
+      name = "avg_#{key}".freeze
+      i_name = "@#{name}".freeze
+      define_method name do
+        if instance_variable_defined?(i_name)
+          instance_variable_get(i_name)
+        else
+          instance_variable_set(i_name, (entries.inject(0) { |m, a| m + a.send(key) } / entries.size).round)
+        end
+      end
+    end
+
+    def to_s
+      "#{hour}\tPing: #{avg_ping}\tDownload: #{avg_download}\tUpload: #{avg_upload}"
+    end
   end
 end
 
@@ -35,7 +45,7 @@ Dir.glob(File.join(File.dirname(__FILE__), '*.txt')) do |file|
   string = IO.read(file)
   next if string.empty? || /Could not/ === string
   $entries << Entry.new(file.tr('_speed.txt', '').tr('/', ''), *$regexps.map do |_k, v|
-      string.match(v).captures.first.to_f
+    string.match(v).captures.first.to_f
   end)
 end
 
