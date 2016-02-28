88fcb9f0ed785c37eba625e7169cc141110a7535
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2016-02-28 10:42:32 +0100

    MOAR DATA

--- include_benchmark.rb
+++ include_benchmark.rb
@@ -1,30 +1,20 @@
 require 'benchmark/ips'
 
-TEST = <<text
+TEST = "
 Ping: 33.286 ms
 Download: 65.48 Mbit/s
 Upload: 46.77 Mbit/s
-text
+".freeze
 
-TEST2 = 'Could not retrieve speedtest.net configuration: <urlopen error [Errno -2] Name or service not known>'
+TEST2 = 'Could not retrieve speedtest.net configuration: <urlopen error [Errno -2] Name or service not known>'.freeze
 
-REGEXP = /Could not/
+REGEXP = /Could not/.freeze
 Benchmark.ips do |x|
-  x.report("include?") do
-    TEST.include? "Could not".freeze
-    TEST2.include? "Could not".freeze
-  end
+  x.report("include?", 'TEST.include?("Could not".freeze);TEST2.include?("Could not".freeze);' * 1000)
 
-  x.report("starts_with") do
-    TEST.start_with? "Could not".freeze
-    TEST2.start_with? "Could not".freeze
-  end
+  x.report("starts_with", 'TEST.start_with?("Could not".freeze); TEST2.start_with?("Could not".freeze);' * 1000)
 
-  x.report("match") do
-    TEST =~ REGEXP
-    TEST2 =~ REGEXP
-
-  end
+  x.report("match", 'TEST =~ REGEXP; TEST2 =~ REGEXP;' * 1000)
 
   # Compare the iterations per second of the various reports!
   x.compare!
