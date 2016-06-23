b91e041d2e0197ef1609b19346a9892de4945eaa
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2016-06-23 21:12:22 +0200

    Improved analyze.rb

--- analyze.rb
+++ analyze.rb
@@ -4,12 +4,12 @@ require 'gruff'
 $regexps = Hash[%i(ping download upload).map { |n| [n, /#{n.to_s.capitalize}: ((\d|\.)+)/] } ].freeze
 
 module MethodCachable
-  def cache_method(name, i_name, block)
+  def cache_method(name, i_name, &block)
     define_method name do
       if instance_variable_defined?(i_name)
         instance_variable_get(i_name)
       else
-        instance_variable_set(i_name, block.call(self))
+        instance_variable_set(i_name, instance_eval(&block))
       end
     end
   end
@@ -21,28 +21,24 @@ Hour = Struct.new(:hour, :entries) do
 
   %w(min max).each do |str|
     $regexps.each_key do |key|
-      cache_method(
-        "#{str}_#{key}",
-        "@#{str}_#{key}",
-        -> s { s.entries.send("#{str}_by") { |n| n.send(key) }.send(key) }
-      )
+      cache_method "#{str}_#{key}", "@#{str}_#{key}" do
+        entries.send("#{str}_by") { |n| n.send(key) }.send(key)
+      end
     end
   end
 
   $regexps.each_key do |key|
-    cache_method(
-      "avg_#{key}",
-      "@avg_#{key}",
-      -> s { s.entries.inject(0) { |m, a| m + a.send(key) } / s.entries.size }
-    )
+    cache_method "avg_#{key}", "@avg_#{key}" do
+      entries.inject(0) { |m, a| m + a.send(key) } / entries.size
+    end
   end
 
-  def to_s
-    @to_s ||= $regexps.keys.each_with_object String.new("#{hour}\t") do |a, m|
+  cache_method 'to_s', '@to_s' do
+    $regexps.keys.each_with_object String.new("#{hour}\t") do |a, m|
       m << sprintf(
         "%s: %.2f(Max/Min: %.2f/%.2f)\t",
         a.to_s.capitalize,
-        *(%w(avg max min).map { |s| self.send("#{s}_#{a}") })
+        *(%w(avg max min).map { |s| send("#{s}_#{a}") })
       )
     end.chomp("\t")
   end
