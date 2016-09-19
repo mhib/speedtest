b603b2c5dd4fd8868047d398a797d0baefae6069
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2016-09-19 22:01:02 +0200

    Changed jekyll config

--- docs/generate_files.rb
+++ docs/generate_files.rb
@@ -13,7 +13,7 @@ layout: month
 title: "#{name}"
 ident: #{sort}
 ---
-<a href="/images/#{name}.png"><img src="/images/#{name}.png" class="mid" alt="Open" /></a>
+<a href="{{'/images/#{name}.png' | prepend: site.baseurl }}"><img src="{{ '/images/#{name}.png' | prepend: site.baseurl }}" class="mid" alt="Open" /></a>
 EOF
 end
 
