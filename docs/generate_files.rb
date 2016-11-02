23f90f179d677c38d532ef56f9784e8fc231cc9f
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2016-11-02 19:44:07 +0100

    Fixed next month link

--- docs/generate_files.rb
+++ docs/generate_files.rb
@@ -6,12 +6,11 @@ DATA_PATH = File.join(File.expand_path(File.dirname(__FILE__)), '..', 'data')
 IMG_PATH = File.join(File.expand_path(File.dirname(__FILE__)), 'images')
 MONTHS = File.join(File.expand_path(File.dirname(__FILE__)), '_months')
 
-def new_view(name, sort)
+def new_view(name)
 <<EOF
 ---
 layout: month
-title: "#{name}"
-ident: #{sort}
+title: #{name}
 ---
 <a href="{{'/images/#{name}.png' | prepend: site.baseurl }}"><img src="{{ '/images/#{name}.png' | prepend: site.baseurl }}" class="mid" alt="Open" /></a>
 EOF
@@ -23,13 +22,12 @@ Dir.foreach(DATA_PATH) do |f|
   @months_set.add(month) if month
 end
 
-
-@months_set.each do |month|
+@months_set.sort_by { |month| month.split('-').reverse.join }.each_with_index do |month, id|
   data = SpeedTest::DataBuilder.new(File.join(DATA_PATH, "*#{month}_speed.txt")).call
   image_path = File.join(IMG_PATH, "#{month}.png")
   SpeedTest::Grapher.new(title: month, hours: data).call(image_path)
-  File.open(File.join(MONTHS, "#{month}.markdown"), 'w') do |f|
-    f.write(new_view(month, month.split('-').reverse.join))
+  File.open(File.join(MONTHS, "#{id}-#{month}.markdown"), 'w') do |f|
+    f.write(new_view(month))
   end
 end
 
